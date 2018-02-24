cf2bf793e3b768ef672101260154fe9152a6dcd9
Denis Matveev <matveev@iskrauraltel.ru>
2018-02-25 02:19:39 +0500

    cross compilation scripts added

--- tor/cross-compile-arm-shared.sh
+++ tor/cross-compile-arm-shared.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export CPPFLAGS="-I/usr/arm-linux-gnueabihf/include/ -I/home/dmat/projects/libs-cross-compilation/include/openssl -I/home/dmat/projects/curl-7.58.0/include"
-export LDFLAGS="-L/usr/arm-linux-gnueabihf/lib"
-./autogen.sh && ./configure --enable-static-tor --with-libm-dir=/usr/arm-linux-gnueabihf/lib/  --with-libevent-dir=/home/dmat/projects/libs-cross-compilation  --enable-static-openssl --with-openssl-dir=/home/dmat/projects/libs-cross-compilation --with-zlib-dir=/home/dmat/projects/libs-cross-compilation --disable-asciidoc --with-libcurl-dir=../curl-7.58.0/lib/.libs/ --with-libpthread-dir=/usr/arm-linux-gnueabihf/lib/ --with-libdl-dir=/usr/arm-linux-gnueabihf/lib/ --host=arm-linux-gnueabihf
-
-./autogen.sh && ./configure --enable-shared-libs --host=arm-linux-gnueabihf --with-libm-dir=/usr/arm-linux-gnueabihf/lib/ --with-libevent-dir=/home/dmat/projects/libs-cross-compilation --enable-static-openssl --with-openssl-dir=/home/dmat/projects/libs-cross-compilation --with-zlib-dir=/home/dmat/projects/libs-cross-compilation --with-libcurl-dir=../curl-7.58.0/lib/.libs/ --with-libpthread-dir=/usr/arm-linux-gnueabihf/lib/ --with-libdl-dir=/usr/arm-linux-gnueabihf/lib/
+export CPPFLAGS="-I/usr/arm-linux-gnueabihf/include/ -I/home/dmat/projects/libs-cross-compilation/include/openssl -I/home/dmat/projects/curl-arm/include"
+export LDFLAGS="-L/usr/arm-linux-gnueabihf/lib -L/home/dmat/projects/libs-cross-compilation"
+./autogen.sh && ./configure --enable-shared-libs --host=arm-linux-gnueabihf 
--- tor/cross-compile-arm-static.sh
+++ tor/cross-compile-arm-static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CPPFLAGS="-I/usr/arm-linux-gnueabihf/include/ -I/home/dmat/projects/libs-cross-compilation/include/openssl -I/home/dmat/projects/curl-7.58.0/include"
+export CPPFLAGS="-I/usr/arm-linux-gnueabihf/include/ -I/home/dmat/projects/libs-cross-compilation/include/openssl -I/home/dmat/projects/curl-arm/include"
 export LDFLAGS="-L/usr/arm-linux-gnueabihf/lib"
-./autogen.sh && ./configure --enable-static-tor --with-libm-dir=/usr/arm-linux-gnueabihf/lib/  --with-libevent-dir=/home/dmat/projects/libs-cross-compilation  --enable-static-openssl --with-openssl-dir=/home/dmat/projects/libs-cross-compilation --with-zlib-dir=/home/dmat/projects/libs-cross-compilation --disable-asciidoc --with-libcurl-dir=../curl-7.58.0/lib/.libs/ --with-libpthread-dir=/usr/arm-linux-gnueabihf/lib/ --with-libdl-dir=/usr/arm-linux-gnueabihf/lib/ --host=arm-linux-gnueabihf
+./autogen.sh && ./configure --enable-static-tor  --with-libevent-dir=/home/dmat/projects/libs-cross-compilation  --enable-static-openssl --with-openssl-dir=/home/dmat/projects/libs-cross-compilation --with-zlib-dir=/home/dmat/projects/libs-cross-compilation --disable-asciidoc --with-libcurl-dir=/home/dmat/projects/curl-arm/lib/.libs/ --with-libpthread-dir=/usr/arm-linux-gnueabihf/lib/ --with-libdl-dir=/usr/arm-linux-gnueabihf/lib/ --with-libm-dir=/usr/arm-linux-gnueabihf/lib/ --with-libc-dir=/usr/arm-linux-gnueabihf/lib/ --with-librt-dir=/usr/arm-linux-gnueabihf/lib/  --host=arm-linux-gnueabihf
 
