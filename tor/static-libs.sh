c4cc74bd3d659af9f8810be143db851f2bddd6da
root <root@vm-3b032fe9.netangels.ru>
2018-03-22 08:50:40 +0000

    option --disable-asserts-in-tests doesn't work

--- tor/static-libs.sh
+++ tor/static-libs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make clean;./autogen.sh && ./configure --enable-static-tor --with-libevent-dir=/usr/lib/x86_64-linux-gnu \
-			    --disable-unittests --disable-asserts-in-tests --disable-system-torrc    \
+			    --disable-unittests --disable-system-torrc    \
                             --enable-static-openssl --with-openssl-dir=/usr/lib/x86_64-linux-gnu \
                             --with-zlib-dir=/usr/lib/x86_64-linux-gnu \
                             --disable-asciidoc --with-libcurl-dir=../curl-x86_64/lib/.libs/ \
