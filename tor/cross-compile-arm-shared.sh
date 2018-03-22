94e586248751f0f9dd31d9fdb6ddcbf8fa35dbb2
root <root@vm-3b032fe9.netangels.ru>
2018-03-22 08:29:34 +0000

    added some options to configure

--- tor/cross-compile-arm-shared.sh
+++ tor/cross-compile-arm-shared.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CPPFLAGS="-I/usr/arm-linux-gnueabihf/include/ -I/home/dmat/projects/libs-cross-compilation/include/openssl -I/home/dmat/projects/curl-arm/include"
 export LDFLAGS="-L/usr/arm-linux-gnueabihf/lib -L/home/dmat/projects/libs-cross-compilation"
-./autogen.sh && ./configure --enable-shared-libs --host=arm-linux-gnueabihf 
+./autogen.sh && ./configure --disable-asciidoc --enable-shared-libs --host=arm-linux-gnueabihf --disable-unittests --disable-asserts-in-tests --disable-system-torrc 
